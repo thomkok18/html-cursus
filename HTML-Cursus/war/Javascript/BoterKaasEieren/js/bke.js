8111e3ce24095b8cbb109ded042fc27c5a7f7a95
thomkok18 <tkok@roc-dev.com>
2017-01-18 12:28:17 +0100

    update

---
+++ HTML-Cursus/war/Javascript/BoterKaasEieren/js/bke.js
@@ -0,0 +1,30 @@
+/***************************************************
+ * bke.js
+ * -------------------------------------------------
+ * In dit bestand staat alle javascript code
+ * om ons spel te laten werken.
+ *
+ **************************************************/
+
+
+
+var btnStartStop;
+
+
+
+window.onload = function() {
+    //Elementen selectern
+    btnStartStop =  document.querySelector('Button');
+
+    //EventListners
+    btnStartStop.addEventListener('click', btnStartReset);
+};
+
+function btnStartReset(event) {
+    if (this.innerText == 'Start spel') {
+        this.innerText = 'Reset spel';
+    } else {
+        this.innerText = 'Start spel';
+    }
+}
+cel =  document.querySelector('Button');
---
+++ HTML-Cursus/war/Javascript/BoterKaasEieren/js/vb.js
@@ -0,0 +1,9 @@
+/**
+ * Gemaakt door Stephan.
+ * JavaScript 2015-2016.
+ */
+
+function print(message) {
+    var div = document.getElementById('output');
+    div.innerHTML = message;
+}
