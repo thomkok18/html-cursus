a444fc0db5d6071ffd1d66d8fdb71572e209ce84
thomkok18 <tkok@roc-dev.com>
2017-01-20 13:16:54 +0100

    update

---
+++ HTML-Cursus/war/HTML/ZeeVrouw/popup.js
@@ -0,0 +1,6 @@
+function function1() {
+    document.getElementById("plaatje").innerHTML = "<img class='showAfbeelding' src='../../../img/Sale%20Dames/dames1.png' alt='Korting'>";
+}
+function function2() {
+    document.getElementById("plaatje").innerHTML = "<img class='showAfbeelding' src='../../../img/Sale%20Dames/dames1-1.png' alt='Korting'>";
+}
