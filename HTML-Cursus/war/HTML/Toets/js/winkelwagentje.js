2cb37c65792cfa4baf9fcf25bf048318bb25e23f
thomkok18 <tkok@roc-dev.com>
2017-01-24 17:43:12 +0100

    update

--- HTML-Cursus/war/HTML/Toets/js/winkelwagentje.js
+++ HTML-Cursus/war/HTML/Toets/js/winkelwagentje.js
@@ -1,13 +1,37 @@
-document.getElementById('cart-hplus').addEventListener('submit', total);
+// * naam: Thom Kok
+// * groep: 6C
+// * datum: 25-1-2017
 
-function total(event) {
+document.getElementById('winkelwagentje').addEventListener('submit', function (event) {
     event.preventDefault();
 
-    var aantal = document.getElementById('txt-value');
+    var aantal1 = parseInt(document.getElementById('aantal1').value) || 0,
+        aantal2 = parseInt(document.getElementById('aantal2').value) || 0,
+        aantal3 = parseInt(document.getElementById('aantal3').value) || 0;
+    var prijs1 = 50.00,
+        prijs2 = 20.00,
+        prijs3 = 40.00;
 
-    if(aantal.value === '' || aantal.value === '0') {
-        alert('Vul het aantal in.');
-
-        aantal.focus();
+    if (document.getElementById('veters').checked == true) {
+        var veters = document.getElementById('veters').value;
+    } else {
+        var veters = 0;
     }
-}
+    if (document.getElementById('beschermers').checked == true) {
+        var beschermers = document.getElementById('beschermers').value;
+    } else {
+        var beschermers = 0;
+    }
+
+    var prijs1Totaal = parseFloat((prijs1 * aantal1) + (aantal1 * veters) + (aantal1 * beschermers)),
+        prijs2Totaal = parseFloat((prijs2 * aantal2) + (aantal2 * veters) + (aantal2 * beschermers)),
+        prijs3Totaal = parseFloat((prijs3 * aantal3) + (aantal3 * veters) + (aantal3 * beschermers));
+
+    var uitkomstPrijs = '\u20AC' + parseFloat(prijs1Totaal + prijs2Totaal + prijs3Totaal).toFixed(2);
+
+    console.log('Verstuurd.');
+    document.getElementById('totaal').innerHTML = uitkomstPrijs;
+    document.getElementById('subtotaal1').innerHTML = 'Subtotaal: \u20AC' + prijs1Totaal.toFixed(2);
+    document.getElementById('subtotaal2').innerHTML = 'Subtotaal: \u20AC' + prijs2Totaal.toFixed(2);
+    document.getElementById('subtotaal3').innerHTML = 'Subtotaal: \u20AC' + prijs3Totaal.toFixed(2);
+})
--- HTML-Cursus/war/HTML/ZeeVrouw/popup.js
+++ HTML-Cursus/war/HTML/ZeeVrouw/popup.js
@@ -1,6 +1,36 @@
 function function1() {
-    document.getElementById("plaatje").innerHTML = "<img class='showAfbeelding' src='../../../img/Sale%20Dames/dames1.png' alt='Korting'>";
+    document.getElementById("plaatje").innerHTML = "<img class='showAfbeelding' src='img/Sale%20Dames/dames1.png' alt='Korting'>";
 }
 function function2() {
-    document.getElementById("plaatje").innerHTML = "<img class='showAfbeelding' src='../../../img/Sale%20Dames/dames1-1.png' alt='Korting'>";
+    document.getElementById("plaatje").innerHTML = "<img class='showAfbeelding' src='img/Sale%20Dames/dames1-1.png' alt='Korting'>";
 }
+
+document.getElementById('winkelwagentje').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    var maatDames1 = parseFloat(149.99) || 0, aantalDames1 = parseFloat(document.getElementById('aantal').value) || 0;
+    var prijs = '\u20AC' + (maatDames1 * aantalDames1).toFixed(2);
+
+    switch (aantalDames1) {
+        case 'één' :
+            aantalDames1 = 1;
+            break;
+        case 'twee' :
+            aantalDames1 = 2;
+            break;
+        case 'drie' :
+            aantalDames1 = 3;
+            break;
+        case 'vier' :
+            aantalDames1 = 4;
+            break;
+        case 'vijf' :
+            aantalDames1 = 5;
+            break;
+        default :
+            aantalDames1 = 0;
+            break;
+    }
+    console.log('verstuurd');
+    document.getElementById('totaal').value = prijs;
+})
