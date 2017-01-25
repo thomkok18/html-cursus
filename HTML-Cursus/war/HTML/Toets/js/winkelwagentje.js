9a2a9d450be1a43709f94babc1a168e7704c42e6
thomkok18 <tkok@roc-dev.com>
2017-01-25 13:42:23 +0100

    update

--- HTML-Cursus/war/HTML/Toets/js/winkelwagentje.js
+++ HTML-Cursus/war/HTML/Toets/js/winkelwagentje.js
@@ -8,9 +8,9 @@ document.getElementById('winkelwagentje').addEventListener('submit', function (e
     var aantal1 = parseInt(document.getElementById('aantal1').value) || 0,
         aantal2 = parseInt(document.getElementById('aantal2').value) || 0,
         aantal3 = parseInt(document.getElementById('aantal3').value) || 0;
-    var prijs1 = 50.00,
-        prijs2 = 20.00,
-        prijs3 = 40.00;
+    var prijs1 = 45,
+        prijs2 = 55,
+        prijs3 = 65;
 
     if (document.getElementById('veters').checked == true) {
         var veters = document.getElementById('veters').value;
@@ -27,10 +27,10 @@ document.getElementById('winkelwagentje').addEventListener('submit', function (e
         prijs2Totaal = parseFloat((prijs2 * aantal2) + (aantal2 * veters) + (aantal2 * beschermers)),
         prijs3Totaal = parseFloat((prijs3 * aantal3) + (aantal3 * veters) + (aantal3 * beschermers));
 
-    var uitkomstPrijs = '\u20AC' + parseFloat(prijs1Totaal + prijs2Totaal + prijs3Totaal).toFixed(2);
+    var uitkomstPrijs = parseFloat(prijs1Totaal + prijs2Totaal + prijs3Totaal).toFixed(2);
 
     console.log('Verstuurd.');
-    document.getElementById('totaal').innerHTML = uitkomstPrijs;
+    document.getElementById('totaal').innerHTML = 'Totaal: \u20AC' + uitkomstPrijs;
     document.getElementById('subtotaal1').innerHTML = 'Subtotaal: \u20AC' + prijs1Totaal.toFixed(2);
     document.getElementById('subtotaal2').innerHTML = 'Subtotaal: \u20AC' + prijs2Totaal.toFixed(2);
     document.getElementById('subtotaal3').innerHTML = 'Subtotaal: \u20AC' + prijs3Totaal.toFixed(2);
