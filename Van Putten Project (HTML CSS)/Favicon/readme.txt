2e5a793887d3ee58c4d459b0b22aa941dc6fbe87
Thomkok <thomkok13@hotmail.com>
2019-08-03 20:04:53 +0200

    Update

---
+++ Van Putten Project (HTML CSS)/Favicon/readme.txt
@@ -0,0 +1,38 @@
+
+* Favicon Generator readme
+
+* Inhoud zip bestand:
+
+ - favicon.ico - favicon.
+ - preview_16x16.png - 16*16 PNG afbeelding van de favicon.
+ - animated_favicon.gif - favicon met de scrollende tekst.
+ - readme.txt - Instructies om de favicon toe te voegen aan uw website.
+ 
+ -------------
+ 
+ Wanneer u de standaard favicon wilt toe voegen aan uw website dan moet u de
+ afbeelding favicon.ico uploaden naar de hoofd folder van uw website.
+ Dit is de folder /www/uw-eigen-domeinnaam.nl/
+ 
+ U kunt de afbeelding uploaden via de filemanager in het control panel.
+ 
+ Alle moderne browsers zullen nu de favicon automatisch weergeven naast de URL in de
+ adres balk van de browser.
+ 
+ -------------
+ 
+ Wilt u de favicon met de scrollende tekst toevoegen aan uw website dan moet u de
+ afbeelding animated_favicon.gif uploaden naar de hoofd folder van uw website.
+ Dit is de folder /www/uw-eigen-domeinnaam.nl/
+ 
+ U kunt de afbeelding uploaden via de filemanager in het control panel.
+ 
+ Daarna moet u de onderstaande HTML code toevoegen tussen de <head> ... </head>
+ tags van uw webpagina's om de favicon zichtbaar te maken:
+ 
+ <link rel="icon" href="animated_favicon.gif" type="image/gif" >
+
+ -------------
+	
+  Your-webhost.nl
+  http://www.your-webhost.nl
---
+++ ZeeVrouw/popup.js
@@ -0,0 +1,29 @@
+function function1() {
+    document.getElementById("plaatje").innerHTML = "<img class='showAfbeelding' src='img/Sale%20Dames/dames1.png' alt='Korting'>";
+}
+function function2() {
+    document.getElementById("plaatje").innerHTML = "<img class='showAfbeelding' src='img/Sale%20Dames/dames1-1.png' alt='Korting'>";
+}
+
+document.getElementById('winkelwagentje').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    var aantal1 = parseInt(document.getElementById('aantal1').value) || 0,
+        aantal2 = parseInt(document.getElementById('aantal2').value) || 0,
+        aantal3 = parseInt(document.getElementById('aantal3').value) || 0;
+    var prijs1 = 45,
+        prijs2 = 55,
+        prijs3 = 65;
+
+    var prijs1Totaal = parseFloat((prijs1 * aantal1)),
+        prijs2Totaal = parseFloat((prijs2 * aantal2)),
+        prijs3Totaal = parseFloat((prijs3 * aantal3));
+
+    var uitkomstPrijs = parseFloat(prijs1Totaal + prijs2Totaal + prijs3Totaal).toFixed(2);
+
+    console.log('Verstuurd.');
+    document.getElementById('totaal').innerHTML = 'Totaal: \u20AC' + uitkomstPrijs;
+    document.getElementById('subtotaal1').innerHTML = 'Subtotaal: \u20AC' + prijs1Totaal.toFixed(2);
+    document.getElementById('subtotaal2').innerHTML = 'Subtotaal: \u20AC' + prijs2Totaal.toFixed(2);
+    document.getElementById('subtotaal3').innerHTML = 'Subtotaal: \u20AC' + prijs3Totaal.toFixed(2);
+})
